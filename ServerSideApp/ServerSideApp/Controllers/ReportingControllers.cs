5627860c6d04578c3456c0b629ae6253f263b56a
jarb99 <76447823+jarb99@users.noreply.github.com>
2024-04-09 12:48:49 +1000

    Report Designer working

--- ServerSideApp/ServerSideApp/Controllers/ReportingControllers.cs
+++ ServerSideApp/ServerSideApp/Controllers/ReportingControllers.cs
@@ -1,10 +1,55 @@
-﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevExpress.AspNetCore.Reporting.QueryBuilder;
+using DevExpress.AspNetCore.Reporting.QueryBuilder.Native.Services;
+using DevExpress.AspNetCore.Reporting.ReportDesigner;
+using DevExpress.AspNetCore.Reporting.ReportDesigner.Native.Services;
+using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
 using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
+using DevExpress.DataAccess.Sql;
+using DevExpress.XtraReports.Web.ReportDesigner.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace  ServerSideApp.Controllers {
+namespace ServerSideApp.Controllers
+{
+    public class CustomReportDesignerController : ReportDesignerController
+    {
+        public CustomReportDesignerController(IReportDesignerMvcControllerService controllerService)
+            : base(controllerService)
+        {
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> GetDesignerModel(
+            [FromForm] string reportName,
+            [FromServices] IReportDesignerModelBuilder reportDesignerModelBuilder
+        ) {
+            var dataSources = new Dictionary<string, object>();
+            var ds = new SqlDataSource("NWindConnectionString");
 
-    public class CustomWebDocumentViewerController : WebDocumentViewerController {
-        public CustomWebDocumentViewerController(IWebDocumentViewerMvcControllerService controllerService) : base(controllerService) {
+            SelectQuery query = SelectQueryFluentBuilder.AddTable("Products").SelectAllColumnsFromTable().Build("Products");
+            ds.Queries.Add(query);
+            ds.RebuildResultSchema();
+            dataSources.Add("Northwind", ds);
+
+            reportName = string.IsNullOrEmpty(reportName) ? "TestReport" : reportName;
+            var designerModel = await reportDesignerModelBuilder
+                .Report(reportName)
+                .DataSources(dataSources)
+                .BuildModelAsync();
+            return DesignerModel(designerModel);
+        }
+    }
+
+    public class CustomQueryBuilderController : QueryBuilderController
+    {
+        public CustomQueryBuilderController(IQueryBuilderMvcControllerService controllerService)
+            : base(controllerService) { }
+    }
+    public class CustomWebDocumentViewerController : WebDocumentViewerController
+    {
+        public CustomWebDocumentViewerController(IWebDocumentViewerMvcControllerService controllerService)
+            : base(controllerService)
+        {
         }
     }
 }
--- react-document-viewer/src/App.js
+++ react-document-viewer/src/App.js
@@ -1,27 +1,44 @@
 import './App.css';
 import { useEffect, useRef } from 'react';
 import { DxReportViewer } from 'devexpress-reporting/dx-webdocumentviewer';
+import { DxReportDesigner } from 'devexpress-reporting/dx-reportdesigner';
 import * as ko from 'knockout';
 
+const ReportDesigner = () => {
+    const reportUrl = ko.observable("TestReport");
+    const designerRef = useRef();
+    const requestOptions = {
+      host: "https://localhost:62359/",
+      getDesignerModelAction: "DXXRD/GetDesignerModel"
+    };
+    useEffect(() => {
+      const designer = new DxReportDesigner(designerRef.current, { reportUrl, requestOptions });
+      designer.render(); 
+      return () => designer.dispose();
+    })
+    return (<div ref={designerRef}></div>);
+}
+
 const ReportViewer = () => {
-  const reportUrl = ko.observable("TestReport");
-  const viewerRef = useRef();
-  const requestOptions = {
-    host: "https://localhost:62359/",
-    invokeAction: "DXXRDV"
-  };
-  useEffect(() => {
-    const viewer = new DxReportViewer(viewerRef.current, { reportUrl, requestOptions });
-    viewer.render(); 
-    return () => viewer.dispose();
-  })
-  return (<div ref={viewerRef}></div>);
+    const reportUrl = ko.observable("TestReport");
+    const viewerRef = useRef();
+    const requestOptions = {
+        host: "https://localhost:62359/",
+        invokeAction: "DXXRDV"
+    };
+    useEffect(() => {
+        const viewer = new DxReportViewer(viewerRef.current, { reportUrl, requestOptions });
+        viewer.render();
+        return () => viewer.dispose();
+    })
+    return (<div ref={viewerRef}></div>);
 }
 
 function App() {
-  return (<div style={{ width: "100%", height: "1000px" }}>
-      <ReportViewer />
-  </div>);
-  }
+    return (<div style={{ width: "100%", height: "1000px" }}>
+        {/* <ReportViewer /> */}
+        <ReportDesigner />
+    </div>);
+}
 
 export default App;
